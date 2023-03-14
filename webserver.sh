6ad86c89ed3c5ebd426408de50cf7006f7967aa9
Stephen Lauck <stephen.lauck@gmail.com>
2023-03-14 15:54:33 -0700

    Add linux support for script and rename.

---
+++ webserver.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# script to create an Ubuntu VM with nginx installed
+
+if [ "$(uname)" == 'Linux' ]
+then
+    echo "i'm on linux"
+    sudo snap install multipass
+elif [ "$(uname)" == 'Darwin' ]
+then
+     echo "i'm on Mac"
+
+     echo "installing brew"
+     NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+     echo "installing multipass"
+     brew install --cask multipass
+     
+fi
+
+echo "launching relativepath instance with multipass"
+multipass launch --name relativepath
+
+echo "show distro information using multipass exec"
+multipass exec relativepath -- lsb_release -a
+
