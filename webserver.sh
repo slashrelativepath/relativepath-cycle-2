3d27a4007a10221e558febe701ba6c27d9b0b4d9
VirtDev337 <337harvey@gmail.com>
2023-03-15 16:30:33 -0600

    Added conditional to check if snapd is installed.  Added multipass commnds to update apt package info. Added installation of nginx server and if installed, start the server.

--- webserver.sh
+++ webserver.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 # script to create an Ubuntu VM with nginx installed
 
+echo "Checking operating system..."
 if [ "$(uname)" == 'Linux' ]
 then
     echo "i'm on linux"
-    if [ -x ! "command -v snapd" ]; then
+    if [ -x ! "command -v snapd" ]; 
+    then
+        echo "installing snapd..."
         sudo apt install snapd
     fi
+    echo "installing multipass..."
     sudo snap install multipass
 elif [ "$(uname)" == 'Darwin' ]
 then
      echo "i'm on Mac"
 
-     echo "installing brew"
+     echo "installing brew..."
      NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-     echo "installing multipass"
+     echo "installing multipass..."
      brew install --cask multipass
      
 fi
@@ -25,4 +29,17 @@ multipass launch --name relativepath
 
 echo "show distro information using multipass exec"
 multipass exec relativepath -- lsb_release -a
+
+echo "update apt for all current package info"
+multipass exec relativepath -- sudo apt update -yq
+echo "installing nginx quietly..."
+multipass exec relativepath -- sudo apt install -yq nginx
+
+if [ multipass exec relativepath -- nginx -v ];
+then
+    multipass exec relativepath -- sudo systemctl start nginx
+else
+    echo "nginx installation failed: exiting"
+    exit 1
+fi
 
