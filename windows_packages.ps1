25627d5fff0c4d04662a202cb200b5a9dd316721
Everett Lee-Wuollet <Everett.leewuollet@gmail.com>
2023-03-15 15:39:11 -0700

    make choco and virtual box install idem potent

--- windows_packages.ps1
+++ windows_packages.ps1
@@ -1,9 +1,24 @@
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
+choco -v
+$Env:returncode = $?
+if ($returncode -eq "False") {
+	iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+else {
+	echo "choco in already installed"
+}	
 
 refreshenv
 
-choco install virtualbox -y
+virtualbox -v
+$Env:returncode = $?
+if ($returncode -eq "False") { 
+	choco install virtualbox -y
+}
+else {
+	echo "Virtual box installed"
+}
 
 choco install multipass -y --params="'/HyperVisor:VirtualBox'"
 
