53b55220b1cce1cfcab899702d0bc879a750f9f3
thatoldplatitude <itsfirstnamedotlastname@tutanota.com>
2023-03-25 00:22:04 -0400

    Add ability to start instance with a bridged network connection. This is a limit of using virtualbox driver not supplying a visible IP address for our instance. Otherwise we wouldnt be able to ssh into our relativepath instance as we can in our bash script.

--- windows_packages.ps1
+++ windows_packages.ps1
@@ -1,4 +1,8 @@
 
+Function Get-Wifi-Network{
+  return multipass networks --format yaml | grep wifi -B 1 | awk -F: 'NR==1{print "$1"}'
+}
+
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
 choco -v
 $Env:returncode = $?
@@ -32,12 +36,12 @@ else {
 refreshenv
 
 multipass set local.driver=virtualbox
-
+Start-Sleep -Seconds 5
 multipass set local.privileged-mounts=1
-
+Start-Sleep -Seconds 5
+$network=Get-Wifi-Network
+multipass set local.bridged-network=$network
 refreshenv
-
 echo "launching relative path instance with Multipass"
-multipass launch --name relativepath0
-
+multipass launch --name relativepath --bridged
 
