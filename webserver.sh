1cd3e617db0b0b4ce7b11c3b73b2f82c91f18715
VirtDev337 <337harvey@gmail.com>
2023-03-27 15:13:44 -0600

    add here-doc

--- webserver.sh
+++ webserver.sh
@@ -40,13 +40,29 @@ else
 	ssh-keygen -f "./ed25519" -t ed25519 -b 4096 -N ''
 fi
 
+if [ -f ./cloud-init.yaml ] #(grep "$(cat ./ed25519.pub)" ./cloud-init.yaml)
+then
+    echo "cloud-init.yaml already exists and is correct"
+else
+    echo "create cloud-init.yaml and add the ssh public key..."
+    cat <<- EOF > "cloud-init.yaml"
+users:
+	- default
+	- name: rp-user
+		sudo: ALL=(ALL) NOPASSWD:ALL
+		shell: /bin/bash
+		ssh_authorized_keys:
+			- $(cat ./ed25519.pub)
+EOF
+fi
+
 echo "launching relativepath instance with multipass"
-if ( multipass info relativepath )
+if ( multipass info relativepath 2> /dev/null )
 then
     echo "relativepath vm already exists!"
 else
     echo "creating relative path vm..."
-    multipass launch --name relativepath
+    multipass launch --name relativepath --cloud-init cloud-init.yaml
 fi
 
 # Add SSH public key to VM
